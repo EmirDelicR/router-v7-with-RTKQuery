ff808d513fc20c4a63f6bbb3db583904d9ad3562
EmirDelicR <emirdelictbf@gmail.com>
2024-12-07 08:16:04 +0100

    Fix test

--- src/feature/todo/TodoList.test.tsx
+++ src/feature/todo/TodoList.test.tsx
@@ -1,39 +1,42 @@
-import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { expect, vi } from "vitest";
-import { renderWithProviders } from "@/utils/test-utils";
-import TodoList from "./TodoList";
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { expect, vi } from 'vitest';
+import { renderWithProviders } from '@/utils/test-utils';
+import TodoList from './TodoList';
 
 const mockNavigate = vi.fn();
 
-vi.mock("react-router", async () => ({
-  ...(await vi.importActual<Record<string, unknown>>("react-router")),
+vi.mock('react-router', async () => ({
+  ...(await vi.importActual<Record<string, unknown>>('react-router')),
   useNavigate: () => mockNavigate,
-  useNavigation: () => ({ state: "idle" }),
+  useNavigation: () => ({ state: 'idle' }),
   useLoaderData: () => [
-    { id: "todo-id", title: "todo title", description: "todo-description" },
+    { id: 'todo-id', title: 'todo title', description: 'todo-description' },
   ],
 }));
 
-describe("<TodoList/>", () => {
+describe('<TodoList/>', () => {
   afterAll(() => {
     vi.restoreAllMocks();
   });
 
-  describe("Layout test", () => {
-    it("should render element", () => {
+  describe('Layout test', () => {
+    it('should render element', () => {
       renderWithProviders(<TodoList />);
 
-      expect(screen.getAllByTestId("list-item").length).toBe(1);
+      expect(screen.getAllByTestId('list-item').length).toBe(1);
     });
 
-    it("should navigate to details page", async () => {
+    it('should navigate to details page', async () => {
       renderWithProviders(<TodoList />);
-      const firstItem = screen.getAllByTestId("list-item")[0]!;
+      const firstItem = screen.getAllByTestId('list-item')[0]!;
 
       await userEvent.click(firstItem);
-      expect(mockNavigate).toHaveBeenCalledWith("todo-id", {
+      expect(mockNavigate).toHaveBeenCalledWith('todo-id', {
         viewTransition: true,
+        state: {
+          page: 1,
+        },
       });
     });
   });
